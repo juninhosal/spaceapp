7f0f147670502e6703941368371c19bd3dbf9d51
Paulo de Tarso Salgado Junior <junior.salgado@outlook.com>
2024-07-14 17:18:47 -0300

    02. Componentes iniciais e fontes

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import {styled} from "styled-components"
+import { EstilosGlobais } from "./componentes/EstilosGlobais"
+import { Cabecalho } from "./componentes/Cabecalho"
+import { BarraLateral } from "./componentes/BarraLateral"
+import { Banner } from "./componentes/Banner"
+import bannerBackground from './assets/banner.png'
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+function App() {
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      <AppContainer>
+        <Cabecalho/>
+          <MainContainer>
+            <BarraLateral/>
+            <ConteudoGaleria>
+              <Banner
+                    texto="A galeria mais completa de fotos do espaço!"
+                    backgroundImage={bannerBackground}
+                  />
+            </ConteudoGaleria>
+          </MainContainer>
+      </AppContainer>
+    </FundoGradiente>
+  )
+}
+
+export default App
